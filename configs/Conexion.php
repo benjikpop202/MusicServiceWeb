19a4bda243e1e839324fcc575620c692171ba787
Gjpiriz <gustavopirizdegeo@gmail.com>
2024-09-05 20:33:14 -0300

    se agrego carpeta configs y model

---
+++ configs/Conexion.php
@@ -0,0 +1,14 @@
+<?php
+$config = [];
+$config['database']['host'] = 'localhost'; // Cambiar esto si el host es diferente
+$config['database']['userName'] = 'postgres';
+$config['database']['password'] = 'Uriel2022'; // Tu contraseña
+$config['database']['databasename'] = 'MusicService'; // mi base de datos
+$config['database']['port'] = '5432'; // Puerto por defecto para PostgreSQL
+//esto es un comentario
+?>
+
+
+
+
+
