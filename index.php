44659b5e22d686bf549eca38bd5c9dd9907babdd
Gjpiriz <gustavopirizdegeo@gmail.com>
2024-09-05 15:57:44 -0300

    Actualización de index.php

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php
-define('SERVERPATH', $_SERVER['DOCUMENT_ROOT']);
+//define('SERVERPATH', $_SERVER['DOCUMENT_ROOT']);
 // Incluye el archivo principal de Smarty. Ajusta la ruta según sea necesario.
-require_once(SERVERPATH.'\libs\Smarty.class.php');
-
+//require_once(SERVERPATH.'\libs\Smarty.class.php');
+require_once('libs\Smarty.class.php');
 // Configuración de la base de datos
 
 $smarty = new Smarty\Smarty;
+
 
 // Configura los directorios de Smarty. Usa __DIR__ para obtener la ruta absoluta
 $smarty->setTemplateDir(__DIR__ . '/templates');
